1cd5cb048b99e6aef025fa9a97b8b3a2984c06bc
Jilani <yazidi.jilani@gmail.com>
2022-07-04 18:43:50 +0200

    hightest value

--- expenses-chart-component-main/index.js
+++ expenses-chart-component-main/index.js
@@ -9,7 +9,36 @@ fetch(url)
         return res.json();
     })
     .then(function(data) {
-        console.log(data[0]['amount'])
-        divChart.style.maxHeight = Math.max(data["amount"]);
+        // console.log(data[0]['amount'])
+        // console.log(data.length);
+        
+        // console.log(Object.values(data));
+
+        // for (const key of Object.values(data)){
+        //     console.log(key)
+        // }
+
+        // console.log(Math.max(...data.map(element => element.amount)));
+
+        // data.forEach(element => {
+        //     console.log(Math.max(element.amount))
+            
+        // });
+
+        // for(let i = 0; i < data.length; i++){
+        //     console.log()
+        // }
+
+        // Object.keys(data).forEach(key => {
+        //     // console.log(key);
+        //     console.log(data[key]);
+        // })
+
+        divChart.style.height = Math.max(...data.map(element => element.amount)) + "px";
         rect1.style.height = data[0]["amount"] + '%';
+
+        //const myDays = [];
+        for(let i = 0; i < data.length; i++){
+            console.log(...data.map(element => element.amount))
+        }
     })
