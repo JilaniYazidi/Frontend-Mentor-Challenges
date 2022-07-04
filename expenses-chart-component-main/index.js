1b1c5eeb08753a031f6fa4d9c0c35f16fbd6d49e
Jilani <yazidi.jilani@gmail.com>
2022-07-04 11:23:43 +0200

    JS v2

--- expenses-chart-component-main/index.js
+++ expenses-chart-component-main/index.js
@@ -1,12 +1,15 @@
 const divChart = document.querySelector('#divChart');
 const url = './data.json';
+const rect1 = document.querySelector('#rect1');
 
 //Import of my datas from my JSON file
 
 fetch(url)
     .then(function(res){
-        return res.text();
+        return res.json();
     })
     .then(function(data) {
-        divChart.textContent = data;
+        console.log(data[0]['amount'])
+        divChart.style.maxHeight = Math.max(data["amount"]);
+        rect1.style.height = data[0]["amount"] + '%';
     })
