a5e193d9df710a8eda17e9f92accc67192de5412
Jilani <yazidi.jilani@gmail.com>
2022-07-18 18:20:00 +0200

    Creating burger menu and styling it

---
+++ ecommerce-product-page-main/scripts/script.js
@@ -0,0 +1,17 @@
+const sideNav = document.getElementById("mySideNav");
+const openBtn = document.getElementById("openBtn");
+const closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  sideNav.classList.add("active");
+  
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  sideNav.classList.remove("active");
+}
