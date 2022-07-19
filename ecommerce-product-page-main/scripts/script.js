efdf686b5e608002acefc7756bace0386dc80c91
Jilani <yazidi.jilani@gmail.com>
2022-07-19 18:19:53 +0200

    Starting carrousel in JS

--- ecommerce-product-page-main/scripts/script.js
+++ ecommerce-product-page-main/scripts/script.js
@@ -1,17 +1,35 @@
-const sideNav = document.getElementById("mySideNav");
-const openBtn = document.getElementById("openBtn");
-const closeBtn = document.getElementById("closeBtn");
+const sideNav = document.querySelector("#mySideNav");
+const openBtn = document.querySelector("#openBtn");
+const closeBtn = document.querySelector("#closeBtn");
+const leftBtn = document.querySelector("#leftBtn");
+const rightBtn = document.querySelector("#rightBtn");
 
 openBtn.onclick = openNav;
 closeBtn.onclick = closeNav;
 
+
+document.body.onload = function(){
+  let nbr = 4;
+  let p = 0;
+  const container = document.getElementById("carrouselContainer");
+  const leftBtn = document.getElementById("leftBtn");
+  const rightBtn = document.getElementById("rightBtn");
+  
+  for(i = 1; i <= nbr; i++ ){
+    div = document.createElement("div");
+    div.className = "shoesPictures";
+    div.style.backgroundImage = "url('images/image-product-"+i+".jpg')";
+    container.appendChild(div);
+  }
+}
+
 /* Set the width of the side navigation to 250px */
 function openNav() {
-  sideNav.classList.add("active");
-  
+  sideNav.classList.add("active");  
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   sideNav.classList.remove("active");
 }
+
