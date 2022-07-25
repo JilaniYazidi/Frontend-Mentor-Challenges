3a9d9d125cda855d46dcc4592d81ca4f53d2eff7
Jilani <yazidi.jilani@gmail.com>
2022-07-25 17:51:30 +0200

    Changing carrousel for something easier. Need to fix it

--- ecommerce-product-page-main/scripts/script.js
+++ ecommerce-product-page-main/scripts/script.js
@@ -1,27 +1,53 @@
 const sideNav = document.querySelector("#mySideNav");
 const openBtn = document.querySelector("#openBtn");
 const closeBtn = document.querySelector("#closeBtn");
+const items = document.querySelectorAll("img");
+const nbSlide = items.length;
 const leftBtn = document.querySelector("#leftBtn");
 const rightBtn = document.querySelector("#rightBtn");
+let count = 0;
+
+// console.log(items.length);
 
 openBtn.onclick = openNav;
 closeBtn.onclick = closeNav;
 
 
-document.body.onload = function(){
-  let nbr = 4;
-  let p = 0;
-  const container = document.getElementById("carrouselContainer");
-  const leftBtn = document.getElementById("leftBtn");
-  const rightBtn = document.getElementById("rightBtn");
+function slideSuivante(){
+  items[count].classList.remove('active');
   
-  for(i = 1; i <= nbr; i++ ){
-    div = document.createElement("div");
-    div.className = "shoesPictures";
-    div.style.backgroundImage = "url('images/image-product-"+i+".jpg')";
-    container.appendChild(div);
+  if(count < nbSlide - 1){
+    count++;
+  } else{
+    count = 0;
   }
+
+  items[count].classList.add('active');
+  console.log(count);
 }
+
+rightBtn.addEventListener('click', slideSuivante);
+
+
+function slidePrecedente(){
+  items[count].classList.remove('active');
+
+  if(count > 0){
+    count --;
+  } else{
+    count = nbSlide - 1;
+  }
+
+  items[count].classList.add('active');
+  console.log(count);
+}
+
+leftBtn.addEventListener('click', slidePrecedente);
+
+
+
+
+
 
 /* Set the width of the side navigation to 250px */
 function openNav() {
